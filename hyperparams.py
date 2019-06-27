f9886971f94c22ed349bc5e648e4600d53745ca1
chuniee0126 <chunie0126@gmail.com>
2019-06-27 17:02:40 +0900

    for attempt2

--- hyperparams.py
+++ hyperparams.py
@@ -4,26 +4,27 @@ class Base:
     batch_size = 128 # initial batch size
     lr = 0.05 # Initial learning rate
     
-    multiples = 1
+    multiples = 7
 
 class Hyperparams:
     '''Hyper parameters'''
     device = [Base.multiples - 1]
+#     device = [7]
     
     batch_size = Base.batch_size * (2 ** (Base.multiples - 1))
-    lr = Base.lr 
+    lr = Base.lr * (2 ** (Base.multiples - 1))
 
     # optim
     momentum = 0.9
     trust_coef = 0.1
     
     # warm-up step & Linear Scaling Rule
-    warmup_multiplier = 2 * (2 ** (Base.multiples - 1)) 
+    warmup_multiplier = 2
     warmup_epoch = 5
     
     # decay lr step
     max_decay_epoch = 200
-    end_learning_rate = 0.0001 * Base.lr * 2
+    end_learning_rate = 0.0001 * Base.lr * 2 * (2 ** (Base.multiples - 1))
 
 #     # step rule (for LR decay)
 #     step_size = 50
@@ -33,10 +34,10 @@ class Hyperparams:
     
     resume = False
     
-    with_lars = False
-    weight_decay = 5e-4
-#     with_lars = True
-#     weight_decay = 5e-3    
+#     with_lars = False
+#     weight_decay = 5e-4
+    with_lars = True
+    weight_decay = 5e-3    
     
     def print_hyperparms():
         print('batch_size: ' + str(Hyperparams.batch_size))
